5106102dd1083ec991682914a15ee487a388074d
lightwo <lightwo@gmail.com>
2009-04-29 17:24:15 +0900

    models.pyにTaskモデルとPileモデルを追加

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=32)
+
+class Pile(models.Model):
+    task = models.ForeignKey(Task)
+    comment = models.CharField(max_length=256)
+    created = models.DateTimeField(auto_now_add=True)
